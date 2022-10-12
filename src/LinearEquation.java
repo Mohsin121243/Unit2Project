1f8d4ece7902a63eb7562d3162441a89b5e8c6b5
Mohsin121243 <mchoudhary4645@bths.edu>
2022-10-12 12:31:32 -0400

    two classes added

---
+++ src/LinearEquation.java
@@ -0,0 +1,9 @@
+public class LinearEquation {
+    //Instance Variable
+    private int x1;
+    private int x2;
+    private int y1;
+    private int y2;
+
+    public LinearEquationCalc()
+}
---
+++ src/Unit2ProjectRunner.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Unit2ProjectRunner {
+    public static void main(String[] args) {
+    Scanner s = new Scanner(System.in);
+    System.out.print("Enter the first x-y coordinate point: ");
+    String firstCoordinatePoint = s.nextLine();
+    System.out.print("Enter the second x-y coordinate point:  ");
+    String secondCoordinatePoint = s.nextLine();
+
+
+    }
+}
