3697d3433598b32765060100133665c67bd39cae
Mohsin121243 <mchoudhary4645@bths.edu>
2022-10-20 00:33:18 -0400

    End of project

--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -5,5 +5,47 @@ public class LinearEquation {
     private int y1;
     private int y2;
 
-    public LinearEquationCalc()
+    private String slopeInFraction;
+    private double regularSlope;
+    public LinearEquation(int x1, int x2, int y1, int y2){
+   double slope = ((double)y2-y1)/(x2-x1);
+   this.regularSlope=slope;
+        double yIntercept= y1 - slope*x1;
+   slope = Math.round(slope*100)/100.0;
+       yIntercept = Math.round(yIntercept*100)/100.0;
+       System.out.println("y = " + (int)(slope*100) + "/" +"100" + "x" + " + "+ yIntercept);
+       this.x1 = x1;
+       this.x2 = x2;
+       this.y1 = y1;
+       this.y2 = y2;
+   }
+
+
+
+ public String slopeCalc(){
+        this.regularSlope= Math.round(regularSlope*100)/100.0;
+     slopeInFraction = (regularSlope*100 + "/" +"100");
+            String slopeFractionn = slopeInFraction;
+        return slopeFractionn;
+
+
+ }
+
+ public double distance(){
+        double distance = Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
+         distance = Math.round(distance*100)/100.0;
+        return distance;
+ }
+public double yIntercept() {
+        double slope = ((double)y2-y1)/(x2-x1);
+    double yIntercept= y1 - slope*x1;
+    yIntercept = Math.round(yIntercept*100)/100.0;
+    return yIntercept;
+}
+public String usersNewCoordinate(double x){
+        double yValue = (regularSlope*x) + yIntercept();
+        String coordinate = "("+x+","+yValue+")";
+        return coordinate;
+}
+
 }
--- src/Unit2ProjectRunner.java
+++ src/Unit2ProjectRunner.java
@@ -3,9 +3,25 @@ public class Unit2ProjectRunner {
     public static void main(String[] args) {
     Scanner s = new Scanner(System.in);
     System.out.print("Enter the first x-y coordinate point: ");
-    String firstCoordinatePoint = s.nextLine();
+    String firstCoordinate = s.nextLine();
     System.out.print("Enter the second x-y coordinate point:  ");
-    String secondCoordinatePoint = s.nextLine();
+    String secondCoordinate = s.nextLine();
+
+    int x1 = Integer.parseInt(firstCoordinate.substring(1,firstCoordinate.indexOf(",")));
+    int y1 = Integer.parseInt(firstCoordinate.substring(firstCoordinate.indexOf(",")+1, firstCoordinate.indexOf(")")));
+    int x2 = Integer.parseInt(secondCoordinate.substring(1,secondCoordinate.indexOf(",")));
+    int y2 = Integer.parseInt(secondCoordinate.substring(secondCoordinate.indexOf(",")+1, secondCoordinate.indexOf(")")));
+        LinearEquation equation = new LinearEquation(x1,x2,y1,y2);
+
+ System.out.println("");
+ System.out.println("Here are the original coordiante point:  First Coordinate: "+ firstCoordinate + "   Second coordiante: " + secondCoordinate);
+System.out.println("Here is the slope:"+equation.slopeCalc());
+System.out.println("Here is the y-intercept "+equation.yIntercept());
+System.out.println("Here is the distance: "+equation.distance());
+System.out.println("Enter an x value to get a full coordinate: ");
+int usersXValues = s.nextInt();
+System.out.println("New coordinate: "+ equation.usersNewCoordinate(usersXValues));
+
 
 
     }
